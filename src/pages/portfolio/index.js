849fa9ee618dec0f2f3b51137fc24e88bc809637
Marc McDougall <marc@kilobytestudios.org>
2022-07-06 13:40:49 -0400

    Only showing projects on homepage that are marked as such in WordPress.

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -61,7 +61,9 @@ export default function Portfolio({ data }) {
 
 export const pageQuery = graphql`
   query {
-  allWpPortfolio(sort: { fields: [date], order: DESC }){
+  allWpPortfolio(
+    filter: {portfolioData: {showOnHomepage: {eq: true}}} 
+    sort: {fields: date, order: DESC}){
     nodes {
       content
       slug
