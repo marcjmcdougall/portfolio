364797b7bd4a97593653bdc64a905c94e6da3ee3
Marc McDougall <marc@kilobytestudios.org>
2023-05-03 20:12:11 -0400

    Fixing favicon resolution issue.

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useLocation } from "@reach/router"
 import { useStaticQuery, graphql } from "gatsby"
-import favicon from '../../img/favicon.png';
+import favicon from '../img/favicon.png';
 
 const SEO = ({ title, description, image, article }) => {
 
