ad9e7c98350ca38789f799d47a609ac53bf9b557
Marc McDougall <marc@kilobytestudios.org>
2024-07-01 19:27:29 -0400

    Adding breadcrumbs.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,7 +2,14 @@
     <div class="container">
         <div class="row">
             <div class="col-8">
-                <a href="{{ route('articles.index') }}">Back to All Articles</a>
+                <ul class="breadcrumbs">
+                    <li class="breadcrumbs__item">
+                        <a href="{{ route('articles.index') }}">Articles</a>
+                    </li>
+                    <li class="breadcrumbs__item">
+                        <a href="{{ route('articles.index') }}">Conversion-Rate Optimization</a>
+                    </li>
+                </ul>
                 <h1>{{ $article->title }}</h1>
                 <div class="article__content">
                     {!! $article->content !!}
