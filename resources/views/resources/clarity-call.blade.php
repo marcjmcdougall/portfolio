0ebfd3944e8475428b2df3714173c6945cd8dce1
Marc McDougall <marc@kilobytestudios.org>
2024-07-03 18:51:23 -0400

    Added resource hero HTML and styles.

--- resources/views/resources/clarity-call.blade.php
+++ resources/views/resources/clarity-call.blade.php
@@ -1,12 +1,40 @@
 <x-base>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <x-page-header 
-                    title="Clarity Call" 
-                    description="Imagine if you could pick my brain for an hour." >
-                    {{-- breadcrumbs="resources"> --}}
-                </x-page-header>
+        <div class="row vcenter resource-hero">
+            <div class="col-6">
+                <div class="resource-hero__text">
+                    <h1 class="resource-hero__title">Tired of prospects <span class="highlight">bouncing</span> without signing up?</h1>
+                    <p class="body--large resource-hero__body">I'll show you exactly why people aren't signing up for your SaaS app.  Here's what you get…</p>
+                    <ul class="normalize-list resource-hero__feature-list">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                                <path d="M20 6L9 17L4 12" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            Pick my brain for up to 90 minutes.
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                                <path d="M20 6L9 17L4 12" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            Clarity on how to get people to subscribe to your software product.
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                                <path d="M20 6L9 17L4 12" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            Actionable steps to fix your funnel.
+                        </li>
+                    </ul>
+                    <div class="button-group">
+                        <a href="#" class="btn btn--secondary">Book a Clarity Call</a>
+                        {{-- <a href="#" class="btn btn--tertiary">Secondary CTA</a> --}}
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 vcenter">
+                <div class="resource-hero__image-wrapper vcenter">
+                    <img class="resource-hero__image" src="{{ asset('img/clarity-call-image.jpg') }}"/>
+                </div>
             </div>
         </div>
         <div class="row">
