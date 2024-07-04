d6388a50642acb6dc992ffb643d4a28efdb71361
Marc McDougall <marc@kilobytestudios.org>
2024-07-03 23:04:23 -0400

    Several typography tweaks.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,12 @@
 <x-base>
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>Welcome to my new portfolio</h1>
-                <p>What would you like to do?</p>
-                <a class="btn btn--secondary" href="{{ route('articles.index') }}">Browse Articles</a>
+            <div class="col-8">
+                <h1>Design that drives <span class="highlight">business results.</span></h1>
+                <p class="body--large">What would you like to do?</p>
+                <div class="button-group">
+                    <a class="btn btn--secondary btn--large" href="{{ route('articles.index') }}">Browse Articles</a>
+                </div>
             </div>
         </div>
     </div>
