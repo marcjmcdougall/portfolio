a539907e2d6f8225a11a1f95398a8bacef1b8f0d
Marc McDougall <marc@kilobytestudios.org>
2024-07-07 14:32:20 -0400

    Added engagement options.

--- resources/views/components/engagement-options.blade.php
+++ resources/views/components/engagement-options.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+<section class="engagement-options">
+    <div class="row">
+        <div class="col-7">
+            <h2>How can I help?</h2>
+            <p>There's 2 ways I can help your business.</p>
+        </div>
+    </div>
+
+    <div class="row engagement-options__items margin-top--sm">
+        <div class="col-6">
+            <a href="{{ route('resources.clarity-call') }}">
+                <div class="engagement-options__featured-image rounded"
+                    style="background-image: url( '{{ asset('img/clarity-call-image.jpg') }}' )" >
+                    <img class="sr-only" src="{{ asset('img/clarity-call-image.jpg') }}" alt="An image depicting a conversation with an expert designer"/>
+                </div>
+                <h3>Clarity Call</h3>
+                <p>There's 2 ways I can help your business.</p>
+            </a>
+        </div>
+        <div class="col-6">
+            <a href="#">
+                <div class="engagement-options__featured-image rounded"
+                    style="background-image: url( '{{ asset('img/clarity-call-image.jpg') }}' )" >
+                    <img class="sr-only" src="{{ asset('img/clarity-call-image.jpg') }}" alt="An image depicting a conversation with an expert designer"/>
+                </div>
+                <h3>Full-service consulting</h3>
+                <p>There's 2 ways I can help your business.</p>
+            </a>
+        </div>
+    </div>
+</section>
