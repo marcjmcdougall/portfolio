34d326d1820be08c060aa6f653ad55ecfd1c48dd
Marc McDougall <marc@kilobytestudios.org>
2025-02-23 18:02:43 -0500

    Adjusting volume

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -20,12 +20,12 @@
                 x-data="{ theme: localStorage.getItem('theme') || 'light' ,
                     playClickOn() {
                             const audio = new Audio('sound/switch-on.wav');
-                            audio.volume = 0.05; // 10% volume
+                            audio.volume = 0.10; // 10% volume
                             audio.play(); 
                     },
                     playClickOff() {
                             const audio = new Audio('sound/switch-off.wav');
-                            audio.volume = 0.05; // 10% volume
+                            audio.volume = 0.10; // 10% volume
                             audio.play(); 
                     }}"
                 x-on:click.prevent="
