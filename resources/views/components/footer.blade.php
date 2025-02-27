c73fec6e80da656621d36e1d7563fa85f553945f
Marc McDougall <marc@kilobytestudios.org>
2025-02-27 13:42:44 -0500

    Updating footer links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,16 +5,19 @@
                 <h4 class="footer__callout">Software looks better when it's moving up and to the right.</h4>
             </div>
             <div class="footer__nav footer__nav--first">
-                <p class="footer__nav__title">About</p>
+                <p class="footer__nav__title">Resources</p>
                 <ul class="normalize-list">
                     <li>
-                        <a href="#">Story</a>
-                    </li>
-                    <li>
-                        <a href="#">Process</a>
+                        <a href="{{ route('articles.index') }}">Articles</a>
                     </li>
                     <li>
                         <a href="{{ route('podcast-appearances.index') }}">Podcast Appearances</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('resources.clarity-call') }}">Clarity Call</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('resources.teardown') }}">Page Teardown</a>
                     </li>
                 </ul>
             </div>
@@ -22,17 +25,20 @@
                 <p class="footer__nav__title">Company</p>
                 <ul class="normalize-list">
                     <li>
-                        <a href="#">Home</a>
+                        <a href="{{ route('index') }}">Home</a>
                     </li>
                     <li>
                         <a href="https://dribbble.com/marcmcdougall" target="_blank">Portfolio</a>
                     </li>
                     <li>
-                        <a href="#">Testimonials</a>
+                        <a href="{{ route('testimonials.index') }}">Testimonials</a>
                     </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
+                    {{-- <li>
+                        <a href="#" 
+                            x-on:click.prevent="Calendly.initPopupWidget({url: 'https://calendly.com/kbs-marc/hello?text_color=353535&primary_color=3a84f3'});return false;">
+                            Let's Talk
+                        </a>
+                    </li> --}}
                 </ul>
                 </div>
         </div>
