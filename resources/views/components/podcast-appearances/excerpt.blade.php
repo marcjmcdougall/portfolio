222e90f586c70642d0908b00b2513bc440e71567
Marc McDougall <marc@kilobytestudios.org>
2025-03-03 13:33:15 -0500

    Adding new read more link to podcast appearances

--- resources/views/components/podcast-appearances/excerpt.blade.php
+++ resources/views/components/podcast-appearances/excerpt.blade.php
@@ -14,5 +14,12 @@
         </div>
         
         <p class="podcast-appearance__excerpt margin-bottom--strip">{{ $podcastAppearance->excerpt }}</p>
+        <p class="article__learn-more">
+            Watch episode
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path d="M3.125 10H16.875" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11.25 4.375L16.875 10L11.25 15.625" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </p>
     </a>
 </article>
