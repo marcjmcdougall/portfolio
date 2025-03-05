a32d56ae18b2b5140fd75be4cc9603a85fb42a0f
Marc McDougall <marc@kilobytestudios.org>
2025-03-05 08:59:31 -0500

    Now using google fonts prefetching

--- config/google-fonts.php
+++ config/google-fonts.php
@@ -4,4 +4,5 @@ return [
     'fonts' => [
         'default' => 'https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap',
     ],
+    'base_url' => 'http://localhost:8000',
 ];
