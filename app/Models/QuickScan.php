37f781b148ed3763698dd7652bb8707bdf0cd315
Marc McDougall <marc@kilobytestudios.org>
2025-03-14 16:28:48 -0400

    Adding fields to model

--- app/Models/QuickScan.php
+++ app/Models/QuickScan.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuickScan extends Model
 {
-    //
+    protected $fillable = [
+        'url',
+        'name',
+        'email',
+        'status',
+        'progress',
+        'html_content',
+        'title',
+        'meta_description',
+        'issues',
+        'score',
+        'completed_at'
+    ];
+
+    protected $casts = [
+        'issues' => 'array',
+        'completed_at' => 'datetime',
+    ];
 }
