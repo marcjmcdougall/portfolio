525d502230a89e574486c1ce0078233c0b391960
Marc McDougall <marc@kilobytestudios.org>
2025-03-14 14:15:12 -0400

    Basic config for scan command

--- app/Console/Commands/Scan.php
+++ app/Console/Commands/Scan.php
@@ -3,28 +3,31 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
 
-class Scan extends Command
+class Scan extends Command implements PromptsForMissingInput
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:scan';
+    protected $signature = 'app:scan
+                                {url : The website URL to scan}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Scans a website for CRO optimization opportunities';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $url = $this->argument('url');
+        $this->info('Scanning ' . $url . ' now...');
     }
 }
