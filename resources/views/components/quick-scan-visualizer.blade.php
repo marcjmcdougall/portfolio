3656ce8766a0c2dc4c05fd0b4486f01e7d25ddd0
Marc McDougall <marc@kilobytestudios.org>
2025-03-22 15:46:24 -0400

    Adding circle animation to report visualizer

--- resources/views/components/quick-scan-visualizer.blade.php
+++ resources/views/components/quick-scan-visualizer.blade.php
@@ -16,5 +16,8 @@
             body="Attention flows randomly" 
             grade="F" delay="0.8"></x-quick-scan-visualizer-item>
         <div class="quick-scan-visualizer__occluder--bottom"></div>
+        <svg class="newsletter-lesson__strikethrough" xmlns="http://www.w3.org/2000/svg" width="427" height="193" viewBox="0 0 427 193" fill="none">
+            <path class="newsletter-lesson__strikethrough__line has-animation" d="M424.952 42.7394C415.041 41.1907 405.223 40.0429 395.189 39.4059C352.952 36.7241 310.165 36.8088 267.897 38.3582C199.235 40.875 132.469 50.8452 69.9826 80.3602C48.4392 90.5361 19.0309 103.619 5.93184 125.124C-3.38656 140.423 3.72391 157.285 17.9801 166.269C33.4015 175.987 51.6537 181.109 69.3159 184.699C104.292 191.807 141.121 193.073 176.607 189.699C211.972 186.336 246.723 178.509 280.564 167.793C310.824 158.211 341.056 145.981 365.235 124.839C378.949 112.847 392.317 98.1333 398.57 80.6936C404.983 62.8073 402.83 40.7829 387.855 27.7386C373.009 14.8064 351.867 9.13853 333.138 5.02322C320.917 2.33777 308.795 1.59448 296.375 1.59448" stroke="#3B84F3" stroke-width="3" stroke-linecap="round"/>
+        </svg>
     </div>
 </div>
