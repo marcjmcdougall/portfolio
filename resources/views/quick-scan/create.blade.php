eacba65fcc44271bf8d44c7e68e7abd8d9cbb1f4
Marc McDougall <marc@kilobytestudios.org>
2025-03-23 21:32:14 -0400

    Added more flair to footer template

--- resources/views/quick-scan/create.blade.php
+++ resources/views/quick-scan/create.blade.php
@@ -1,58 +1,11 @@
-<x-base> 
+<x-base  hideNewsletter> 
     <div class="container">
         <section class="quick-scan-hero">
             <div class="row">
                 <div class="quick-scan-hero__text col-7">
                     <h1 class="text--hero margin-top--strip">Unlock serious <span class="highlight">revenue</span> in your landing page.</h1>
                     <p class="body--large">Scan your landing page for hidden conversion-rate optimization opportunuties.</p>
-                    <form action="/resources/quick-scan" method="POST" class="form">
-                        @csrf
-
-                        <!-- URL Input -->
-                        <div class="form-item">
-                            <label class="form-group__label" for="url">Landing Page</label>
-                            <input type="text" value="{{ old('url') }}" id="url" name="url" required placeholder="https://example.com">
-                            @error('url')
-                                <p>{{ $message }}</p>
-                            @enderror
-                        </div>
-                        
-                        <!-- Email Input -->
-                        <div class="form-item">
-                            <label class="form-group__label" for="email">Your Email</label>
-                            <input type="email" value="{{ old('email') }}" id="email" name="email" required placeholder="Email">
-                            @error('email')
-                                <p>{{ $message }}</p>
-                            @enderror
-                        </div>
-                        
-                        <!-- Consent Checkbox -->
-                        <div>
-                            <div class="form-item--checkbox">
-                                <input type="checkbox" id="consent" name="consent" required>
-                                <label for="consent">
-                                    I agree to receive occasional (very fun) marketing emails.
-                                </label>
-                            </div>
-                            @error('terms')
-                                <p>{{ $message }}</p>
-                            @enderror
-                        </div>
-
-                        @error('limit_reached')
-                            <div class="alert alert-warning">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                        
-                        <!-- Submit Button -->
-                        <div class="quick-scan-hero__actions">
-                            <div class="glimmer-container">
-                                <button class="btn btn--secondary btn--large quick-scan-hero__button homepage-hero__btn" type="submit">Analyze Landing Page</button>
-                            </div>
-                            <p class="newsletter-opt-in__aside margin-bottom--strip">100% free.  Limited to 1 analysis per day.</p>
-                        </div>
-                    </form>
+                    <x-quick-scan-form></x-quick-scan-form>
                 </div>
                 <div class="col-5 vcenter">
                     <x-quick-scan-visualizer></x-quick-scan-visualizer>
@@ -97,6 +50,8 @@
         </section>
 
         <x-about-me></x-about-me>
+
+        <x-quick-scan-opt-in></x-quick-scan-opt-in>
 
     </div>
 </x-base>
