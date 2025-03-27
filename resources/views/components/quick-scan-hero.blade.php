8b400248e3e8c0f0e8f60f251546c704d893b9e6
Marc McDougall <marc@kilobytestudios.org>
2025-03-27 14:42:12 -0400

    Created new templates for quick scan and homepage heros

---
+++ resources/views/components/quick-scan-hero.blade.php
@@ -0,0 +1,13 @@
+<section class="quick-scan-hero">
+    <div class="row">
+        <div class="quick-scan-hero__text col-7">
+            <h1 class="text--hero margin-top--strip">Unlock serious <span class="highlight">revenue</span> in your landing page.</h1>
+            <p class="body--large">Scan your landing page for hidden conversion-rate optimization opportunuties.</p>
+            <x-quick-scan-form></x-quick-scan-form>
+        </div>
+        <div class="col-5 vcenter">
+            <x-quick-scan-visualizer></x-quick-scan-visualizer>
+            {{-- <x-impact-visualizer></x-impact-visualizer> --}}
+        </div>
+    </div>
+</section>
--- resources/views/quick-scan/create.blade.php
+++ resources/views/quick-scan/create.blade.php
@@ -1,19 +1,6 @@
 <x-base  hideNewsletter> 
     <div class="container">
-        <section class="quick-scan-hero">
-            <div class="row">
-                <div class="quick-scan-hero__text col-7">
-                    <h1 class="text--hero margin-top--strip">Unlock serious <span class="highlight">revenue</span> in your landing page.</h1>
-                    <p class="body--large">Scan your landing page for hidden conversion-rate optimization opportunuties.</p>
-                    <x-quick-scan-form></x-quick-scan-form>
-                </div>
-                <div class="col-5 vcenter">
-                    <x-quick-scan-visualizer></x-quick-scan-visualizer>
-                    {{-- <x-impact-visualizer></x-impact-visualizer> --}}
-                </div>
-            </div>
-        </section>
-
+        <x-quick-scan-hero></x-quick-scan-hero>
         <div class="row margin-top--md">
             <div class="col-12">
                 <div class="separator">
