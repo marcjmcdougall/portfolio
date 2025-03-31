5f3bb9d07d820ce0f40727e8b3d9be665fc4f56e
Marc McDougall <marc@kilobytestudios.org>
2025-03-31 15:52:12 -0400

    Added responsive styles for quickscan create form

--- resources/views/components/quick-scan-form.blade.php
+++ resources/views/components/quick-scan-form.blade.php
@@ -1,48 +1,52 @@
-    <form action="/resources/quick-scan" method="POST" class="form">
-        @csrf
+@php
+    $formId = Str::uuid();
+@endphp
 
-        <!-- URL Input -->
-        <div class="form-item">
-            <label class="form-group__label" for="url">Landing Page</label>
-            <input type="text" value="{{ old('url') }}" id="url" name="url" required placeholder="https://example.com">
-            @error('url')
-                <p>{{ $message }}</p>
-            @enderror
-        </div>
-        
-        <!-- Email Input -->
-        <div class="form-item">
-            <label class="form-group__label" for="email">Your Email</label>
-            <input type="email" value="{{ old('email') }}" id="email" name="email" required placeholder="Email">
-            @error('email')
-                <p>{{ $message }}</p>
-            @enderror
-        </div>
-        
-        <!-- Consent Checkbox -->
-        <div>
-            <div class="form-item--checkbox">
-                <input type="checkbox" id="consent" name="consent" required>
-                <label for="consent">
-                    I agree to receive occasional (very fun) marketing emails.
-                </label>
-            </div>
-            @error('terms')
-                <p>{{ $message }}</p>
-            @enderror
-        </div>
+<form action="/resources/quick-scan" method="POST" class="form" id="{{ $formId }}">
+    @csrf
 
-        @error('limit_reached')
-            <div class="alert alert-warning">
-                {{ $message }}
-            </div>
+    <!-- URL Input -->
+    <div class="form-item">
+        <label class="form-group__label" for="url">Landing Page</label>
+        <input type="text" value="{{ old('url') }}" id="url" name="url" required placeholder="https://example.com">
+        @error('url')
+            <p>{{ $message }}</p>
         @enderror
-        
-        <!-- Submit Button -->
-        <div class="quick-scan-hero__actions">
-            <div class="glimmer-container">
-                <button class="btn btn--secondary btn--large quick-scan-hero__button homepage-hero__btn" type="submit">Analyze Landing Page</button>
-            </div>
-            <p class="newsletter-opt-in__aside margin-bottom--strip">100% free.  Limited to 1 analysis / day.</p>
+    </div>
+    
+    <!-- Email Input -->
+    <div class="form-item">
+        <label class="form-group__label" for="email">Your Email</label>
+        <input type="email" value="{{ old('email') }}" id="email" name="email" required placeholder="Email">
+        @error('email')
+            <p>{{ $message }}</p>
+        @enderror
+    </div>
+    
+    <!-- Consent Checkbox -->
+    <div>
+        <div class="form-item--checkbox">
+            <input type="checkbox" id="consent--{{ $formId }}" name="consent" required>
+            <label for="consent--{{ $formId }}">
+                I agree to receive occasional (very fun) marketing emails.
+            </label>
         </div>
-    </form>
+        @error('terms')
+            <p>{{ $message }}</p>
+        @enderror
+    </div>
+
+    @error('limit_reached')
+        <div class="alert alert-warning">
+            {{ $message }}
+        </div>
+    @enderror
+    
+    <!-- Submit Button -->
+    <div class="quick-scan-hero__actions">
+        <div class="glimmer-container">
+            <button class="btn btn--secondary btn--large quick-scan-hero__button homepage-hero__btn" type="submit">Analyze Landing Page</button>
+        </div>
+        <p class="newsletter-opt-in__aside margin-bottom--strip">100% free.  Limited to 1 analysis / day.</p>
+    </div>
+</form>
--- resources/views/components/quick-scan-hero.blade.php
+++ resources/views/components/quick-scan-hero.blade.php
@@ -1,7 +1,7 @@
 <section class="quick-scan-hero">
     <div class="row">
         <div class="quick-scan-hero__text col-7">
-            <h1 class="text--hero margin-top--strip">Unlock serious <span class="highlight">revenue</span> in your landing page.</h1>
+            <h1 class="text--hero">Unlock serious <span class="highlight">revenue</span> in your landing page.</h1>
             <p class="body--large">Scan your landing page for hidden conversion-rate optimization opportunuties.</p>
             <x-quick-scan-form></x-quick-scan-form>
         </div>
