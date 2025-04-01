f868b5ca3fe2ab09326a627f84d95226b7750f3a
Marc McDougall <marc@kilobytestudios.org>
2025-04-01 12:05:59 -0400

    Minor style changes for 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,7 +1,7 @@
 <x-base hideNewsletter>
     <div class="container not-found">
         <h1 class="text--bold strip--mt h2">Page not found</h1>
-        <p class="body--large" style="max-width: 550px;">Sorry, but the page you were looking for could not be found.  Perhaps there was a typo?</p>
+        <p class="body--large strip--mt" style="max-width: 550px;">Sorry, but the page you were looking for could not be found.  Perhaps there was a typo?</p>
         <a href="/" class="btn btn--tertiary margin-top--sm">Return To Home</a>
     </div>
 </x-base>
