5af95c4ff8de9d90567c388815999ec9e9611046
Marc McDougall <marc@kilobytestudios.org>
2025-04-01 12:03:14 -0400

    404 page styles

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,5 +1,5 @@
 <x-base hideNewsletter>
-    <div class="container">
+    <div class="container 404">
         <h1 class="text--bold strip--mt h2">Page not found</h1>
         <p class="body--large" style="max-width: 550px;">Sorry, but the page you were looking for could not be found.  Perhaps there was a typo?</p>
         <a href="/" class="btn btn--tertiary margin-top--sm">Return To Home</a>
