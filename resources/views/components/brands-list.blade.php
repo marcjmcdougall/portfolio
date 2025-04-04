25893cf26eb8ec8875ef80d34c628fc0acbf8295
Marc McDougall <marc@kilobytestudios.org>
2025-04-04 15:45:28 -0400

    Hiding extra brand items for static version

--- resources/views/components/brands-list.blade.php
+++ resources/views/components/brands-list.blade.php
@@ -15,7 +15,7 @@
         {{-- Output twice for animation --}}
         @foreach( $brands as $brand )
             {{-- <div class="brands-list__item" src="background-image: url( 'storage/{{ $brand->image }}' )"></div> --}}
-            <img class="brands-list__item lazy" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
+            <img class="brands-list__item brands-list__item--extra lazy" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
         @endforeach
     </div>
     <div class="brands-list__occluder brands-list__occluder--right"></div>
