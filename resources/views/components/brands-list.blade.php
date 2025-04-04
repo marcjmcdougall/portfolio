3360967b14aff752c130c421001eba78e44c19d3
Marc McDougall <marc@kilobytestudios.org>
2025-04-04 16:08:52 -0400

    Now only lazy loading if not animating

--- resources/views/components/brands-list.blade.php
+++ resources/views/components/brands-list.blade.php
@@ -9,13 +9,13 @@
     <div class="brands-list__wrapper">
         @foreach( $brands as $brand )
             {{-- <div class="brands-list__item" src="background-image: url( 'storage/{{ $brand->image }}' )"></div> --}}
-            <img class="brands-list__item lazy" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
+            <img class="brands-list__item {{ $animated ? '' : 'lazy' }}" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
         @endforeach
 
         {{-- Output twice for animation --}}
         @foreach( $brands as $brand )
             {{-- <div class="brands-list__item" src="background-image: url( 'storage/{{ $brand->image }}' )"></div> --}}
-            <img class="brands-list__item brands-list__item--extra lazy" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
+            <img class="brands-list__item brands-list__item--extra {{ $animated ? '' : 'lazy' }}" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
         @endforeach
     </div>
     <div class="brands-list__occluder brands-list__occluder--right"></div>
