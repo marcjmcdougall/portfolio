9aca354980d61e68f445a2dc297ef43b1a4ce883
Marc McDougall <marc@kilobytestudios.org>
2025-04-04 16:10:34 -0400

    Update brands-list.blade.php

--- resources/views/components/brands-list.blade.php
+++ resources/views/components/brands-list.blade.php
@@ -9,13 +9,29 @@
     <div class="brands-list__wrapper">
         @foreach( $brands as $brand )
             {{-- <div class="brands-list__item" src="background-image: url( 'storage/{{ $brand->image }}' )"></div> --}}
-            <img class="brands-list__item {{ $animated ? '' : 'lazy' }}" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
+            <img class="brands-list__item {{ $animated ? '' : 'lazy' }}" 
+                data-created-at="{{ $brand->created_at }}" 
+                alt="{{ $brand->name }} logo"
+                @if ($animated)
+                    data-src="/storage/{{ $brand->image }}"
+                @else
+                    src="/storage/{{ $brand->image }}"
+                @endif
+                />
         @endforeach
 
         {{-- Output twice for animation --}}
         @foreach( $brands as $brand )
             {{-- <div class="brands-list__item" src="background-image: url( 'storage/{{ $brand->image }}' )"></div> --}}
-            <img class="brands-list__item brands-list__item--extra {{ $animated ? '' : 'lazy' }}" data-created-at="{{ $brand->created_at }}" alt="{{ $brand->name }} logo" data-src="/storage/{{ $brand->image }}"/>
+            <img class="brands-list__item brands-list__item--extra {{ $animated ? '' : 'lazy' }}" 
+                data-created-at="{{ $brand->created_at }}"
+                alt="{{ $brand->name }} logo"
+                @if ($animated)
+                    data-src="/storage/{{ $brand->image }}"
+                @else
+                    src="/storage/{{ $brand->image }}"
+                @endif
+                />
         @endforeach
     </div>
     <div class="brands-list__occluder brands-list__occluder--right"></div>
