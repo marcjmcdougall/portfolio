ddb72a2e6124c8a03d3758682eb05a245219d1da
Marc McDougall <marc@kilobytestudios.org>
2025-04-07 13:07:18 -0400

    Adding log statement

--- app/Livewire/QuickScanReport.php
+++ app/Livewire/QuickScanReport.php
@@ -52,6 +52,8 @@ class QuickScanReport extends Component
             
             // Logs event to Fathom Analytics
             $this->dispatch('scan-finished', eventName: config('services.fathom.event_name' ));
+
+            Log::info('Dispatching Fatom event for ' . $this->quickScan->domain);
             
             // Update status
             $this->quickScan->update([
