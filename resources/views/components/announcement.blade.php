ba270616238b946e6dcda1c4da391dfde2996748
Marc McDougall <marc@kilobytestudios.org>
2025-04-07 16:36:50 -0400

    Changes to announcement CSS

--- resources/views/components/announcement.blade.php
+++ resources/views/components/announcement.blade.php
@@ -18,8 +18,9 @@
         <p>Scan your website for <strong>instant</strong> CRO opportunities</p>
         <div class="announcement__svg__container">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                <path d="M7.5 3.75L13.75 10L7.5 16.25" stroke="#020202" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+  <path d="M3.125 10H16.875" stroke="#020202" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M11.25 4.375L16.875 10L11.25 15.625" stroke="#011627" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
         </div>
     </a>
 </div>
