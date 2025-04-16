3dd004bd1dbf067e8fbbc99b39fe2be073209a0e
Marc McDougall <marc@kilobytestudios.org>
2025-04-16 13:35:50 -0400

    Minor font adjustments

--- config/google-fonts.php
+++ config/google-fonts.php
@@ -3,6 +3,7 @@
 return [
     'fonts' => [
         'default' => 'https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap',
+        // 'default' => 'https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap'
     ],
     'base_url' => 'http://localhost:8000',
 ];
