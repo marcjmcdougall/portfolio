41f404a5293bc8bb82cf7274d5ae9c9ba696ca1d
Marc McDougall <marc@kilobytestudios.org>
2025-08-12 13:26:58 -0400

    Added responsive styles

--- resources/views/components/showcase-list.blade.php
+++ resources/views/components/showcase-list.blade.php
@@ -1,4 +1,8 @@
 <section class="showcase row vcenter margin-top--md margin-bottom--sm">
+    <div class="showcase__occluder__wrap">
+        <div class="showcase__occluder showcase__occluder--left"></div>
+        <div class="showcase__occluder showcase__occluder--right"></div>
+    </div>
     <div class="showcase__row">
         {{-- Loop 3 times for "endless" animation --}}
         @for($i = 0; $i < 3; $i++)
