9b3a31cadbdcc79a2ce8f97b45c68b95e0b92e08
Marc McDougall <marc@kilobytestudios.org>
2025-10-13 16:49:44 -0400

    Initial skeleton

---
+++ resources/views/components/functional-visualizer.blade.php
@@ -0,0 +1,3 @@
+<div class="functional-visualizer" x-data="{ showVideo: true }">
+    <p>Vis here</p>
+</div>
--- resources/views/resources/functional.blade.php
+++ resources/views/resources/functional.blade.php
@@ -1,13 +1,138 @@
-<x-base>
+<x-base 
+    title="Marc McDougall – Design clean software interfaces in minutes"
+    description="Book a 90-minute clarity call to uncover exactly why people aren't signing up for your software product and get actionable conversion tips."
+    ogTitle="Tired of traffic bouncing without signing up? Let's talk."
+    ogDescription="Book a 90-minute clarity call to uncover exactly why people aren't signing up for your software product and get actionable conversion tips."
+    hideNewsletter>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <x-page-header 
-                    title="Function.al" 
-                    description="Spend less time designing, and more time delivering value." >
-                    {{-- breadcrumbs="resources"> --}}
-                </x-page-header>
+        <div class="row vcenter resource-hero">
+            <div class="col-7">
+                <div class="resource-hero__text">
+                    {{-- <h1 class="resource-hero__title text--hero">Tired of traffic <span class="colorize underline has-animation">bouncing</span> before sign-up?</h1> --}}
+                    <h1 class="resource-hero__title text--hero">Design clean interfaces in <span class="colorize underline">minutes.</span></h1>
+                    <p class="body--large resource-hero__body">The Functional design system will help you ship faster, better software interfaces.</p>
+                    <ul class="normalize-list list--feature">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M20 6L9 17L4 12" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            150+ UI Components
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M20 6L9 17L4 12" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            Clarity on how to make your interface simpler, or more clear.
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M20 6L9 17L4 12" stroke="#3A84F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            Actionable steps to fix your design.
+                        </li>
+                    </ul>
+                    <div class="button-group">
+                        <div class="button-wrap" x-data>
+                            <div class="glimmer-container">
+                                <span class="blob"></span>
+                                <a href="#" 
+                                x-on:click.prevent="Calendly.initPopupWidget({url: 'https://calendly.com/kbs-marc/clarity-call?text_color=353535&primary_color=3a84f3'});return false;" 
+                                class="btn btn--secondary btn--large">
+                                    Get Functional Now
+                                </a>
+                            </div>
+                        </div>
+                        {{-- <a href="#" class="btn btn--tertiary">Secondary CTA</a> --}}
+                    </div>
+                    <p class="newsletter-opt-in__aside margin-bottom--strip">Limited availability.  100% refund guarantee.</p>
+                </div>
+            </div>
+            <div class="col-5 vcenter">
+                <div class="resource-hero__image-wrapper resource-hero__image-wrapper--left vcenter center">
+                    <x-functional-visualizer></x-functional-visualizer>
+                </div>
             </div>
         </div>
+        <div class="row margin-top--md">
+            <div class="col-12">
+                <div class="separator">
+                    <span class="separator__line"></span>
+                    <label class="separator__text section-label">What my customers say</label>
+                    <span class="separator__line"></span>
+                </div>
+            </div>
+        </div>
+        <div class="row margin-top--xs padding-bottom-sm">
+            <x-testimonials type="clarity-call" limit="2" showPhoto="true" showRole="true"></x-testimonials>
+        </div>
+
+        <div class="row vcenter margin-top--lg padding-bottom--md">
+            <div class="col-6">
+                <h2 class="strip--mt text--bold">Design doesn't have to be hard.</h2>
+                <p class="strip--mb">Once you deeply understand your customer's needs, landing pages write themselves.</p>
+                <p class="strip--mb">But&hellip;it's tough to understand their needs if you're too in the weeds.</p>
+                <p class="strip--mb"><strong>Sometimes, you just need an external perspective.</strong></p>
+            </div>
+            <div class="col-6">
+                <x-benefit-item text="Uncertain of how to have an impact" type="negative" />
+                <x-benefit-item text="Hard to measure campaign success" type="negative" />
+                <x-benefit-item text="No A/B testing" type="negative" />
+                <x-benefit-item text="Wasting advertising dollars" type="negative" />
+            </div>
+        </div>
+
+        <div class="row vcenter padded">
+            <div class="col-6">
+                <x-impact-visualizer type="comparison" :showStats="false" graphTitle="Conversion funnel for {{ date('F Y') }}" graphValue="42165" graphValuePrefix="" />
+            </div>
+            <div class="col-6">
+                <h2 class="strip--mt text--bold">What if you could <span class="colorize underline has-animation">double</span> your trial sign-ups?</h2>
+                <p class="strip--mb">You could always spend your way out of a bad landing page.</p>
+                <p class="strip--mb">Throw enough money at it and eventually <em>someone</em> will sign up, right?</p>
+                <p class="strip--mb">Or&hellip; you could get some guidance from someone that's done this <strong>hundreds of times.</strong></p>
+            </div>
+        </div>
+
+        <x-about-me></x-about-me>
+
+        <div class="bg--gray padded rounded row vcenter margin-top--lg margin-bottom--md">
+            <div class="col-7">
+                <h2 class="h3 strip--mt newsletter-opt-in__title">Ready to chat?</h2>  
+                <p class="">You could spend another week or so worrying about what to do.  Or&hellip;I could show you <strong>exactly</strong> what you need to do in an hour.</p>
+                {{-- <ul>
+                    <li>Add checkmarks to this perhaps?</li>
+                    <li>Money-back guarantee</li>
+                    <li>Availability status / calendar</li>
+                    <li>FAQ</li>
+                </ul> --}}
+
+                <div class="button-group">
+                    <div class="button-wrap" x-data>
+                        <div class="glimmer-container glimmer-container--light">
+                            <span class="blob"></span>
+                            <a href="#" 
+                                x-on:click.prevent="Calendly.initPopupWidget({url: 'https://calendly.com/kbs-marc/clarity-call?text_color=353535&primary_color=3a84f3'});return false;" 
+                                class="btn btn--secondary">
+                                    Book A Clarity Call
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <p class="newsletter-opt-in__aside margin-top--xxs margin-bottom--strip">Limited availability. 100% refund guarantee.</p>
+            </div>
+            <div class="col-5">
+                <x-testimonials :limit="1" :showPhoto="true" :showRole="true" type="clarity-call"></x-testimonials>
+            </div>
+        </div>
+
+        <div class="row padded margin-bottom--lg">
+            <div class="col-12">
+                <h2 class="h3 strip--mt">Still have questions?</h2>
+                <div class="questions">
+                    <x-questions topic="clarity-call"></x-questions>
+                </div>
+            </div>
+        </div>
+
     </div>
 </x-base>
