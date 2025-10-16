c6bb2e44e0dc4b3885907f7ef49f6dbd7968a409
Marc McDougall <marc@kilobytestudios.org>
2025-10-16 16:26:53 -0400

    Added varying height to chart bars

---
+++ resources/views/components/functional-components/bar-chart.blade.php
@@ -0,0 +1,81 @@
+<div class="functional-visualizer__component">
+    <div class="func-bar-chart">
+        <p class="func-bar-chart__stat">
+            21.2K 
+            <span class="func-bar-chart__stat__delta">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                    <path d="M3 9L9 3" stroke="#16C47F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M4.125 3H9V7.875" stroke="#16C47F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                19.3%
+            </span>
+        </p>
+        <p class="func-bar-chart__byline">Total active users</p>
+        <p class="func-bar-chart__body">This is some longer text that gives the larger statistic and the title some more context.</p>
+        <div class="func-bar-chart__chart">
+            <div class="func-bar-chart__chart__bars">
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+                <div class="func-bar-chart__chart__bars__bar">
+                    <span class="func-bar-chart__chart__bars__bar__tertiary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__secondary"></span>
+                    <span class="func-bar-chart__chart__bars__bar__primary"></span>
+                </div>
+            </div>
+            <div class="func-bar-chart__chart__labels">
+                <p class="func-bar-chart__chart__label">{{ date('d M')}}</p>
+                <p class="func-bar-chart__chart__label">{{ date('d M', strtotime('+1 week')) }}</p>
+            </div>
+            <div class="func-bar-chart__chart__key">
+                <p class="func-bar-chart__chart__key__item"><span class="func-bar-chart__chart__key__swatch func-bar-chart__chart__key__swatch--primary"></span>Customers</p>
+                <p class="func-bar-chart__chart__key__item"><span class="func-bar-chart__chart__key__swatch func-bar-chart__chart__key__swatch--secondary"></span>Leads</p>
+                <p class="func-bar-chart__chart__key__item"><span class="func-bar-chart__chart__key__swatch func-bar-chart__chart__key__swatch--tertiary"></span>Prospects</p>
+            </div>
+        </div>
+    </div>
+    <p class="functional-visualizer__component__label">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <g opacity="0.6">
+                <path d="M10.0001 1.87492L6.875 5L10.0001 8.12508L13.1252 5L10.0001 1.87492Z" stroke="#011627" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10.0001 11.8749L6.875 15L10.0001 18.1251L13.1252 15L10.0001 11.8749Z" stroke="#011627" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M15.0001 6.87492L11.875 10L15.0001 13.1251L18.1252 10L15.0001 6.87492Z" stroke="#011627" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M5.00008 6.87492L1.875 10L5.00008 13.1251L8.12516 10L5.00008 6.87492Z" stroke="#011627" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+        </svg>
+        <span class="subdued">Data Display /</span>
+        Bar Chart
+    </p>
+</div>
--- resources/views/components/functional-visualizer.blade.php
+++ resources/views/components/functional-visualizer.blade.php
@@ -1,6 +1,6 @@
 <div class="functional-visualizer" x-data="{ showVideo: true }">
     <div class="functional-visualizer__components">
-        <x-functional-components.chat-message-input></x-functional-components.chat-message-input>
-        <x-functional-components.chat-message-input></x-functional-components.chat-message-input>
+        {{-- <x-functional-components.chat-message-input></x-functional-components.chat-message-input> --}}
+        <x-functional-components.bar-chart></x-functional-components.bar-chart>
     </div>
 </div>
